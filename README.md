efa1aa4b71fa033429f45824891ee37bd4fce753
betamoreira <79289691+betamoreira@users.noreply.github.com>
2022-03-11 14:19:54 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C22---Invas-o-Pirata
+# PRO-C22-SA-Boilerplate_code
+Boilerplate code for student  
---
+++ sketch.js
@@ -0,0 +1,25 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+
+function preload() {
+  
+}
+
+function setup() {
+
+  canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  
+
+}
+
+function draw() {
+  background(0);
+  Engine.update(engine);
+
+  
+}
